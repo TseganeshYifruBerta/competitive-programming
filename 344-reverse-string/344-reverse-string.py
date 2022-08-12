74aaa9450c2238009cbb56250bf702d093f709c7
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2022-08-12 14:40:34 +0300

    Time: 287 ms (60.62%), Space: 18.3 MB (87.15%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        length = len(s) - 1
+        for i in range(len(s) // 2):
+            s[i], s[length - i] = s[length - i], s[i]
+        
