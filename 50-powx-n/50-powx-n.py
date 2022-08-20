2e75b71d316dac82567735ecbb4ae93b1c3c62b0
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2022-08-20 08:15:40 +0300

    Time: 54 ms (34.53%), Space: 14 MB (19.97%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,13 +1,12 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        @cache
+        @lru_cache(None)
         def rec(n):
-            if n == 1: return x
+            if n <= 1:
+                return x
             if n % 2:
-                return rec(n//2) * rec(n//2) * rec(1)
-            else: 
-                return rec(n//2) * rec(n // 2)
+                return rec(n // 2) * rec(n // 2) * x
+            return rec(n // 2) * rec(n // 2)
         if n == 0: return 1
         if n < 0: return 1/rec(-n)
         return rec(n)
-    
