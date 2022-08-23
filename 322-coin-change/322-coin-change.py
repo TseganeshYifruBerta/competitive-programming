0286489c9536daca1b2a7f2a15795026f1054052
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2022-08-23 22:00:09 +0300

    Time: 3483 ms (11.40%), Space: 38 MB (11.83%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,17 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        @lru_cache(None)
+        def minCoin(coin):
+            if coin < 0: return -1
+            if coin == 0: return 0
+            count = inf
+            for i in range(len(coins)):
+                temp = 1 + minCoin(coin - coins[i])
+                if temp == 0: 
+                    continue
+                count = min(count, temp)
+            if count == inf:
+                return -1
+            return count
+        return minCoin(amount)
+                
