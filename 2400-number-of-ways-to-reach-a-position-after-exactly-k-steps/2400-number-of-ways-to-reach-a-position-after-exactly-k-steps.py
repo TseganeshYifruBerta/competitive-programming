44fc0e163bf9aa36ba77feddd9b50a23dd4c2231
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2022-09-06 23:23:10 +0300

    Time: 2633 ms (53.48%), Space: 648 MB (24.99%) - LeetHub

--- 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.py
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.py
@@ -1,18 +1,13 @@
 class Solution:
     def numberOfWays(self, startPos: int, endPos: int, k: int) -> int:
         M = [10**9+7]
-        dp = {}
+        @lru_cache(None)
         def helper(start, rem):
             if rem == 0:
                 if  start == endPos:
                     return 1
                 return 0
-            if (start, rem) not in dp:
-                left, right = 0, 0
-                if rem >= abs(endPos - start):
-                    left = helper(start - 1, rem - 1)
-                    right = helper(start + 1, rem - 1)
-                dp[(start, rem)] = left + right
-            return dp[(start, rem)] % M[0]
-        
+            left = helper(start - 1, rem - 1)
+            right = helper(start + 1, rem - 1)
+            return (left  + right) % M[0]
         return helper(startPos, k)
