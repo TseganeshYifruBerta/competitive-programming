5e000a1eb95bfcf032b0ae1435eb4cf1cc7bb83b
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2022-09-12 21:43:28 +0300

    Time: 439 ms (10.58%), Space: 14.5 MB (48.31%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -7,20 +7,14 @@ class Solution:
                 l += 1
                 r -= 1
             return True
-        left, right, flag = 0, len(s) - 1, True
-        while right >= left:
-            if s[left] != s[right]:
-                res = [left, right]
-                flag = False
+        
+        l, r, res = 0, len(s) - 1, [0, 0]
+        while r >= l:
+            if s[l] != s[r]:
+                res = [l, r]
                 break
-            left += 1
-            right -= 1
-        if not flag:
-            ans1 = isPal(res[0] + 1, res[1])
-            ans2 = isPal(res[0], res[1] - 1)
-            if ans1 or ans2: 
-                return True
-            else:
-                return False
-        return True
+            l += 1
+            r -= 1
+            
+        return isPal(res[0] + 1, res[1]) or isPal(res[0], res[1] - 1)
                 
