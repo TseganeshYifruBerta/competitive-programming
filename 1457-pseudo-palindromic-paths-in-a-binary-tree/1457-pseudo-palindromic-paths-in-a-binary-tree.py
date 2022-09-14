af61ae3dec0628e6cfd391549f21cde9d5cceb34
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2022-09-14 09:47:46 +0300

    Time: 2803 ms (5.30%), Space: 106.2 MB (5.30%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -7,22 +7,25 @@
 class Solution:
     def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
         self.count = 0
-        def helper(root, d):
+        def helper(root, freq):
+#     everytime increment the frequency of that number by one
+            freq[root.val] += 1
+    
+    
+#     if it is leaf we have to check whether one of its permutation can form a palindromic path or not
+#     easily we can check if their is more than one odd frequency it means this path cannot for a palindrom if not we can count as one valid path
             if root.left == root.right:
-                d[root.val] += 1
                 c = 0
-                for i in d:
-                    if d[i] % 2 :
-                        c += 1
-                    if c > 1:
-                        return 
+                for i in freq:
+                    if freq[i] % 2 : c += 1
+                    if c > 1: return 
+                    
                 self.count += 1
                 return 
+#     if it has left node or right node we call it recursively 
+            if root.left : helper(root.left, freq.copy())
+            if root.right: helper(root.right, freq.copy())
             
-            d[root.val] += 1
-            if root.left :
-                helper(root.left, d.copy())
-            if root.right:
-                helper(root.right, d.copy())
+#     finaly we can call our helper function and return our count
         helper(root, defaultdict(int))
         return self.count
