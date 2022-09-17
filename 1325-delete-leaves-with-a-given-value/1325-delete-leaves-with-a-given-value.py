5891192aebdba09f71e09059182a3f20ff905555
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2022-09-17 12:43:12 +0300

    Time: 124 ms (6.10%), Space: 14.6 MB (83.06%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.py
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.py
@@ -7,26 +7,38 @@
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
         
-        
-        def helper(root):
-            if root.left == root.right:
-                if root.val == target:
-                    return False
+#         check wether the leaf is equal to the target if so we have to delete it(return True)
+        def check(root):
+            if root.left == root.right and root.val == target:
                 return True
             
             
-            if root.left and not helper(root.left):
+        def dfs(root):
+		
+# if root.val equals to target and root is leaf node return False
+            if check(root): return False
+            
+			
+# if root.val is not equal to target and root is leaf node return True
+            if root.left == root.right:
+                return True
+            
+# if root.left is leaf and it's val is equal to target root.left = None(delete the node)
+            if root.left and not dfs(root.left):
                 root.left = None
-            if root.right and not helper(root.right):
+            
+# if root.right is leaf and it's val is equal to target root.left = None(delete the node)
+            if root.right and not dfs(root.right):
                 root.right = None
                 
-                
-            if root.left == root.right and root.val == target:
-                return False
+# if we delete both right and left then we have to check root.val, if it is equal to target the we have to delete that node too
+            if check(root): return False
+			
             return True
         
-        
-        helper(root)
-        if root.left == root.right and root.val == target:
-            return 
+# calling the function
+        dfs(root)
+		
+# after doing all we have to check if the root (if it is leaf and its val == target) then we have to delete it too
+        if check(root): return
         return root
