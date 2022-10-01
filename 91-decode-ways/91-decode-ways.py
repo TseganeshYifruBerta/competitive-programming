4e9dcb3696f92559ee6e5d21f9d8f58d3ad78318
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2022-10-01 06:46:39 +0300

    Time: 46 ms (71.08%), Space: 14.2 MB (19.74%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -2,11 +2,19 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         @lru_cache(None)
         def dp(idx):
+#             basecases
             if idx >= len(s): return 1
             if s[idx] == '0': return 0
-            one, two = 0, 0
+#          if peeking two digits is invalid their has to be default value
+            two = 0
+            
+#             peeking one digit is always valid b/c the value doesn't exceed 9
             one = dp(idx + 1)
+    
+#     peeking two digits if it is valid
             if idx + 1 < len(s) and int(s[idx] + s[idx + 1]) <= 26:
                 two = dp(idx + 2)
+            
+#             return the summation
             return one + two
         return dp(0)
