5aefef5185a54bc32737a2b2d495210bdc537654
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2022-10-03 15:49:40 +0300

    Time: 789 ms (20.25%), Space: 16.4 MB (82.02%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        p1, p2 = 0, 0               
+        totalCus = 0                 
+        for i in range(len(customers)):
+            if grumpy[i] == 0:
+                totalCus += customers[i]
+
+        cusNum, maxx, l = 0, 0, 0
+        
+        
+        for r in range(len(customers)):
+            cusNum = cusNum + customers[r] if grumpy[r] == 1 else cusNum
+            while r - l + 1 == minutes:
+                maxx = max(maxx,cusNum)
+                cusNum = cusNum - customers[l] if grumpy[l] == 1 else cusNum
+                l += 1
+        
+        return totalCus+maxx
