17b58c88ce19cbd407cc2eaba489acab7b48b784
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2022-10-23 06:29:52 +0300

    Time: 382 ms (64.67%), Space: 15.8 MB (41.20%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,16 +1,13 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        lost = None
-        duplicate = None
-        set_duplicate = set()
-        for i in range(len(nums)):
-            if nums[i] not in set_duplicate:
-                set_duplicate.add(nums[i])
-            else:
-                duplicate = nums[i]
+        n = len(nums)
         
-        for i in range(1, len(nums) + 1):
-            if i not in set_duplicate:
-                lost = i
-                
+# the whole sum minus the non-duplicated sum gives as the duplicate value
+        duplicate = sum(nums) - sum(set(nums))
+    
+# the arithimetic sum 1 - n
+        arth_sum = int((((n + 1) / 2) * n)) 
+    
+# we can get the lost one by subtracting the non-duplicated to the arth_sum
+        lost = arth_sum - sum(set(nums))
         return [duplicate, lost]
