0ff5e3f5f15582f715aec0ac5fb909a432e252f9
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2023-02-01 22:47:06 +0300

    Update 2136-earliest-possible-day-of-full-bloom.py

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.py
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.py
@@ -1,7 +1,18 @@
 class Solution:
     def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
-        prevPlant, ans = 0, 0
-        for grow, plant in sorted(zip(growTime, plantTime), reverse=True):
-            ans = max(ans, (grow + plant + prevPlant))
-            prevPlant += plant
-        return ans
+        pots = []
+        for i in range(len(plantTime)):
+            pots.append((growTime[i], plantTime[i]))
+        pots.sort(reverse = True)
+
+
+        total_day = 0
+        answer = 0
+
+        for i in range(len(pots)):
+
+            total_day += pots[i][1]
+
+            answer = max(total_day + pots[i][0], answer)
+
+        return answer
