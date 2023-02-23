2097a443cc037369956e6eb4e92bf953fc7fc1a9
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2023-02-23 23:31:50 +0300

    Time: 35 ms (77.25%), Space: 14.1 MB (43.92%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        
+        def clac(a, b, operator):
+            if operator == "+": return a + b
+            if operator == "-": return a - b
+            if operator == "*": return a * b
+            
+        memo = {}        
+        def solve(expr):
+            if expr.isdigit(): return [int(expr)]
+            if expr in memo: return memo[expr]
+            res = []
+            for i in range(len(expr)):
+                if expr[i] in "+-*":
+                    left = solve(expr[:i])
+                    right = solve(expr[i+1:])
+                    for a in left:
+                        for b in right:
+                            res.append(clac(a, b, expr[i]))
+            memo[expr] = res
+            return memo[expr]
+        
+        return solve(expression)
