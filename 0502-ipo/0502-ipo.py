67ccd13c41ee51e90c003abbdca06a16fb5501dd
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2023-02-23 16:13:51 +0300

    Time: 1291 ms (27.97%), Space: 37 MB (93.71%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,28 +1,22 @@
 class Solution:
-    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        
-        def addCapital(w, heap, capital_profit):
-            while capital_profit and capital_profit[-1][0] <= w:
+    def addCapital(self, w, heap, capital_profit):
+        while capital_profit and capital_profit[-1][0] <= w:
                 c, p = capital_profit.pop()
                 heappush(heap, -p)
                 
-            
-            
-            
-        capital_profit = []
-        for i in range(len(profits)):
-            capital_profit.append([capital[i], profits[i]])
+                
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        capital_profit = [[capital[i], profits[i]] for i in range(len(profits))]
+        
         capital_profit.sort(reverse = True)
         
-        
-        
         heap = []
-        addCapital(w, heap, capital_profit)
+        self.addCapital(w, heap, capital_profit)
+        
         while heap and k > 0:
             prof = heappop(heap)
             w -= prof
-            
-            addCapital(w, heap, capital_profit)
+            self.addCapital(w, heap, capital_profit)
             k -= 1
             
         return w
